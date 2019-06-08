5d8223e009bfa653acf2490aa0210ed607baefda
André Leoni <andreluizleoni@gmail.com>
2019-06-07 22:19:00 -0300

    test kubectl on deploy-production.sh

--- deploy-production.sh
+++ deploy-production.sh
@@ -14,14 +14,14 @@ gcloud components update kubectl
 
 TAG=`git rev-parse --short HEAD`
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin=
+# echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin=
 
-docker build -t andreleoni/inbox-microservice:$TAG .
+# docker build -t andreleoni/inbox-microservice:$TAG .
 
-IMAGE_ID=`docker images | grep andreleoni\/inbox-microservice | head -n1 | awk '{print $3}'`
-docker tag $IMAGE_ID andreleoni/inbox-microservice:latest
+# IMAGE_ID=`docker images | grep andreleoni\/inbox-microservice | head -n1 | awk '{print $3}'`
+# docker tag $IMAGE_ID andreleoni/inbox-microservice:latest
 
-docker push andreleoni/inbox-microservice:$TAG
-docker push andreleoni/inbox-microservice:latest
+# docker push andreleoni/inbox-microservice:$TAG
+# docker push andreleoni/inbox-microservice:latest
 
 cat kube/deployment.yaml | sed s/\$\$DOCKER_IMAGE/andreleoni\\/inbox-microservice:$TAG/g | kubectl apply -f -
