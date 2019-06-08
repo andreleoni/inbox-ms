730a219d12c1b444dea004fd4d048faaf801f935
André Leoni <andreluizleoni@gmail.com>
2019-06-07 21:50:32 -0300

    Travis to continuous integration

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+# Make sure we have gcloud installed in travis env
+if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then
+  rm -rf "$HOME/google-cloud-sdk"
+  curl https://sdk.cloud.google.com | bash > /dev/null
+fi
+
+# Promote gcloud to PATH top priority (prevent using old version fromt travis)
+source $HOME/google-cloud-sdk/path.bash.inc
+
+# Make sure kubectl is updated to latest version
+gcloud components update kubectl
 
 TAG=`git rev-parse --short HEAD`
 
