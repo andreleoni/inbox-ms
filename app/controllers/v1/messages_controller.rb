00c1706ef1dd0699a66437357ae0343855c75d8b
André Leoni <andreluizleoni@gmail.com>
2019-06-07 21:38:10 -0300

    Add new message to test deploy

--- app/controllers/v1/messages_controller.rb
+++ app/controllers/v1/messages_controller.rb
@@ -9,7 +9,8 @@ class V1::MessagesController < ApplicationController
     [
       { message_id: 2, title: "Mensagem de teste" },
       { message_id: 3, title: "Outra mensagem de teste" },
-      { message_id: 4, title: "Ainda outra mensagem de teste" }
+      { message_id: 4, title: "Ainda outra mensagem de teste" },
+      { message_id: 5, title: "Ainda outra mensagem de teste" }
     ]
   end
 end
