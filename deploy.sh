7b019c71f59e0fbb867568ba12e7a744c0ff345b
André Leoni <andreluizleoni@gmail.com>
2019-06-07 21:39:40 -0300

    Deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-rake deploy:start
+TAG=`git rev-parse --short HEAD`
+
+docker build -t andreleoni/inbox-microservice:$TAG .
+
+IMAGE_ID=`docker images | grep andreleoni\/inbox-microservice | head -n1 | awk '{print $3}'`
+docker tag $IMAGE_ID andreleoni/inbox-microservice:latest
+
+docker push andreleoni/inbox-microservice:$TAG
+docker push andreleoni/inbox-microservice:latest
+
+cat kube/deployment.yaml | sed s/\$\$DOCKER_IMAGE/andreleoni\\/inbox-microservice:$TAG/g | kubectl apply -f -
